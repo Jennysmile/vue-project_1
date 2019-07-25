b5003279451a22daf1b93c66639e9ab7e8e17503
Jiawen <jiawen.zhang@andaptchina.com>
2019-07-25 14:46:57 +0800

    the transfer of the params data and query data

--- vue-cart/src/views/Page1.vue
+++ vue-cart/src/views/Page1.vue
@@ -1,12 +1,17 @@
 <template>
     <div>
-        page1
+        <h3>page1</h3>
+        <p><button @click='goToPage2'>跳转到page2</button></p>
     </div>
 </template>
 
 <script>
     export default {
-        
+        methods: {
+            goToPage2() {
+                this.$router.push({name:'page2',params:{id:1,msg:'vuejs'}})
+            }
+        },
     }
 </script>
 
--- vue-cart/src/views/Page2.vue
+++ vue-cart/src/views/Page2.vue
@@ -1,12 +1,16 @@
 <template>
     <div>
         <h3>page2</h3>
+        <p>{{$route.params.id}}</p>
         <p>{{$route.params.msg}}</p>
     </div>
 </template>
 
 <script>
     export default {
+        created(){
+            console.log(this.$route.query)
+        }
         
     }
 </script>
